4298899548ee4d2c8bcaad7fecf11c45a4a6c689
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2024-12-10 19:30:58 -0300

    tabuada

--- cursojs/aula14ex/ex017/script.js
+++ cursojs/aula14ex/ex017/script.js
@@ -6,10 +6,19 @@ function clicar(){
     let num = document.getElementById('txtn')
     let btn = document.getElementById('btn')
     let tab = document.getElementById('txttab')
+    let text = document.getElementById('text')
+    let c = 1
 
     if (num.value.length == 0) {
         window.alert('Comando inválido! Digite um número.')
     } else {
         let n = Number(num.value)
+        text.innerHTML = `Tabuada do <strong>${n}</strong>: </br></br>`
+    
+        for (let c = 0; c <= 10; c++) {
+            let res = n * c
+            text.innerHTML += `${n} x ${c} = <strong>${res}</strong></br>`
+        }  
     }
+    num.value = '' 
 }
