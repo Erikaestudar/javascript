d4bbffd0ca8e64679cb910f6bc795690c3772b3a
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-01-20 16:54:28 -0300

    ex018

--- cursojs/aula16ex/ex018/script.js
+++ cursojs/aula16ex/ex018/script.js
@@ -6,35 +6,62 @@ addBtn.addEventListener('click', adicionar)
 finBtn.addEventListener('click', finalizar)
 limBtn.addEventListener('click', limpar)
 
+let num = document.querySelector('input#fnum'); // Seleciona o campo de input com id 'num'
 
-// Seleciona o campo de número e a lista de resultados
-let selRes = document.querySelector('select#res');
+let lista = document.querySelector('select#flista'); // Seleciona o campo de lista com os resultados dos números
+let res = document.querySelector('div#res'); // Seleciona o campo dos resultados
+let valores = [] //Cria de forma global um vetor
+
+//Para fazer a adição dos elementos é preciso fazer 2 funções
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <=100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
 
 // Função para validar o campo de número
 function adicionar() {
-    let num = document.querySelector('input#num'); // Seleciona o campo de input com id 'num'
+    // Só vai adicionar se ele for um número e se não tiver na lista.
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('ok')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
 
-    let selRes = document.querySelector('select#res');
-
+    /*
     if (num.value.length === 0) {
         window.alert('Comando inválido! Digite um número.'); // Alerta se o campo estiver vazio
+
     } else {
+
         let n = Number(num.value) // Converte o valor para número
 
         let item = document.createElement('option')
-        item.text = `Adicionou o número ${n}`
+        item.text = `Valor ${n} adicionado.`
         item.value = `num${n}`
         selRes.appendChild(item)
     }  
 
+
     num.value = ''; // Limpa o campo de número
-    num.focus(); // Foca no campo para nova entrada
+    num.focus(); // Foca no campo para nova entrada   
+    //  */
 }
 
 
 // Função para limpar a lista
 function limpar() {
-    selRes.innerHTML = ''; // Remove todas as opções da lista
+    lista.innerHTML = ''; // Remove todas as opções da lista
     window.alert('Lista limpa com sucesso! 😊');
 }
 
