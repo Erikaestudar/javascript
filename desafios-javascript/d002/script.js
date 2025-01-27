400bd96157d9e37be7a4320a95364364d3146c82
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-01-27 00:19:34 -0300

    d002
    Mudando o scopo;
    adicionando novas validações;

--- desafios-javascript/d002/script.js
+++ desafios-javascript/d002/script.js
@@ -1,15 +1,31 @@
-const btn = document.querySelector('button.btn')
+let btn = document.querySelector('button.btn')
 btn.addEventListener('click', clicar)
 
 function clicar() {
-    const nome = prompt('Qual é o seu nome?')
-    const idade = Number(prompt(`Olá, ${nome.toUpperCase()}! Quantos anos você tem?`))
+    let nome = prompt('Qual é o seu nome?')
+
+    // Verifica s o prompt não está vazio
+    if (nome.trim() !== "") {
+        let idade = Number(prompt(`Olá, ${nome.toUpperCase()}! Quantos anos você tem?`))
+        
+        if (nome.trim() !== '' && Number(idade) == 0) {
+            alert (`Acabei de conhecer ${nome.toUpperCase()} , cuja a idade é desconhecida. \u{1F609}`)
+        } else if (nome.trim() !== '' && Number(idade) == idade) {
+            alert (`Acabei de conhecer ${nome.toUpperCase()} , que tem ${idade} anos de idade. \u{1F60A}`)
+        } else {
+            alert (`[ERRO] Você digitou letras no campo da idade.`)
+        }
     
-    if (nome == '' || Number(idade) == 0) {
-        alert (`Acabei de conhecer ${nome.toUpperCase()}, cuja a idade deixaremos no anonimato.`)
-    } else if (Number(idade) == idade) {
-        alert (`Acabei de conhecer ${nome.toUpperCase()}, que tem ${idade} anos de idade.`)
     } else {
-        alert (`[ERRO] Você digitou letras no campo da idade.`)
+        let idade = Number(prompt(`Olá, Estranho! Quantos anos você tem?`))    
+        
+        if (nome.trim() == '' && Number(idade) == 0) {
+            alert (`Acabei de conhecer um Estranho, cuja a idade é desconhecida.`)
+        } else if (nome.trim() == '' && Number(idade) == idade) {
+            alert (`Acabei de conhecer um Estranho, que tem ${idade} anos de idade. `)
+        } else {
+            alert (`[ERRO] Você digitou letras no campo da idade.`)
+        }
     }
 }
+ 
