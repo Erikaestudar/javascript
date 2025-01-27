657a842df1e4e468fb2a08f4edb260b39cce11a5
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-01-27 00:20:26 -0300

    Update 003.js
    transformando em números inteiros.

--- desafios-javascript/d003/003.js
+++ desafios-javascript/d003/003.js
@@ -1,13 +1,14 @@
 const submitButton = document.querySelector("#submit-button");
 
-submitButton.addEventListener("click", (event)=>{
+submitButton.addEventListener("click", (event)=> {
     const num = Number (prompt ("Digite um número inteiro", ""));
     const add = num + 1;
     const sub = num - 1;
     
-    if ( Number(num) == num){
+    // Validar apenas números inteiros
+    if ( Number.parseInt(num) == num) { 
     alert ("Você digitou o número: " + num + "!\n" + "Seu Antecessor é: " + sub + "!\n" + "Seu Sucessor é: " + add + "!");
-}else{    
+    } else {    
     alert ("Esse comando não é válido!\nFavor digitar um número inteiro!")
-}
+    }
 });
