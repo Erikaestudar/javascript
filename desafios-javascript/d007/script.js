2a80ca8328aa1fc487724bd5c6098b078efc2e5f
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-01-30 18:53:10 -0300

    d007

---
+++ desafios-javascript/d007/script.js
@@ -0,0 +1,48 @@
+let button = document.querySelector('input#button')
+let res = document.querySelector('div#res')
+
+let dolar = Number(prompt('Antes de mais nada. Quanto está a cotação do dólar agora?'))
+
+// Verifica se a cotação digitada é um número válido
+if (isNaN(dolar) || dolar <= 0) {
+    alert('[ERRO] Valor inválido! Digite um valor válido!')
+    marcarErro()
+} else {
+    button.addEventListener('click', clicar)
+}
+
+// Função que será chamada ao clicar no botão
+function clicar() {
+    let real = Number(prompt('Quantos R$ você deseja converter?'))
+
+    if (isNaN(real) || real <= 0) {
+        alert('[ERRO] Valor inválido! ')
+        marcarErro()
+    } else {
+        res.innerHTML = `<p>
+        <img src="images/flag-brazil.png"> 
+        <img src="images/trending_flat_black.png"> 
+        <img src="images/flag-united-states.png">
+        </p>`
+        
+        res.innerHTML += `<p>Valor R$ ${(real).toFixed(2)} / cotação ${dolar}</p>`
+        res.innerHTML += `<p> Deu US$ ${(real / dolar).toLocaleString('pt-BR', {style: 'currency', currency: 'USD'})}</p>`
+
+        // Remove o erro caso o valor seja válido
+        limparErro()
+    }
+}
+
+// Função para mudar a cor do botão quando houver erro
+function marcarErro() {
+    button.style.backgroundColor = 'red'
+    button.style.color = 'white'
+    button.style.border = '2px solid darkred'
+}
+
+// Função para restaurar a cor normal do botão
+function limparErro() {
+    button.style.backgroundColor = ''
+    button.style.color = ''
+    button.style.border = ''
+}
