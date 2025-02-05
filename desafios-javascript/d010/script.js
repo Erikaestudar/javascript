8384d19f6d5877087736754866afdc28a7469a59
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-02-05 20:44:47 -0300

    Update script.js

--- desafios-javascript/d010/script.js
+++ desafios-javascript/d010/script.js
@@ -7,10 +7,11 @@ let cS = document.getElementById('cSide')
 
 button.addEventListener('click', clicar)
 
-function clicar() {
+function clicar() {        
     // Precisa estar como string para verificar se está vazia.
     if (aS.value === "" || bS.value === "" || cS.value === "") {
        alert('[Erro] Preencha todos os campos!') 
+
     } else {
         // Convertendo para número depois de validar
         a = Number(aS.value)
@@ -18,19 +19,36 @@ function clicar() {
         c = Number(cS.value)
 
         if (isNaN(a) || a == 0) {
-            alert('[Erro] Digite um valor válido nos campos!')
+            alert('[Erro] Digite um valor válido no campo a!')
             aS.value = ""
             aS.focus()
-        } else if (isNaN(b) || b == 0){
-            alert('[Erro] Digite um valor válido nos campos!')
+        } else if (isNaN(b)){
+            alert('[Erro] Digite um valor válido no campo b!')
             bS.value = ""
             bS.focus()
-        } else if (isNaN(c) || c == 0){
-            alert('[Erro] Digite um valor válido nos campos!')
+        } else if (isNaN(c)){
+            alert('[Erro] Digite um valor válido no campo c!')
             cS.value = ""
             cS.focus()
         } else {
+            disBlockBtn()
             alert('ok')
         }
     }
+}
+
+function marcarErro() {
+    button.style.backgroundColor = 'red'
+    button.style.border = '3px solid red'
+    blockBtn()
+}
+
+function blockBtn() {
+    button.disable = 'true'
+}
+
+function disBlockBtn() {
+    button.style.backgroundColor = ''
+    button.style.border = ''
+    button.disable = 'false'
 }
