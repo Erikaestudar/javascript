1eaefe0e0554c3b157c274b0d214d5c45fa4fd7f
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-05-12 23:21:19 -0300

    Create 01-variaveis.js

---
+++ class-room/aulas-02/01-variaveis.js
@@ -0,0 +1,21 @@
+TERMINAL - NODE
+
+// variável sem valor
+var user
+console.log(user)
+
+user = 'erika'
+
+// variável com valor
+var email = "erika@email.com"
+console.log(email,user)
+
+// substitui o valor da variável
+email = "maria@email.com"
+console.log(email)
+
+
+let nome = "Erika"
+console.log(nome)
+
+const pi = 3.141592653589793238462643383279502
