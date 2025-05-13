99ba964b90529699fa750ab802991092c2d8823e
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-05-12 23:21:21 -0300

    Create 03-scopo.js

---
+++ class-room/aulas-02/03-scopo.js
@@ -0,0 +1,21 @@
+// escopo global
+var name = "Erika"
+
+{   
+    // escopo de bloco
+    console.log(name)
+}
+
+// HOISTING - IÇAR
+{
+    var age = 18
+}
+console.log(age)
+
+
+// Com o let não é possível isso
+let address = "Rua X"
+{
+    address = "Rua Y"
+    console.log(address)
+}
