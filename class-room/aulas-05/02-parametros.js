29e4699050eba4dc6631cc1e3ebe47b57681ebd0
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-05-16 09:42:07 -0300

    parametros e return

--- class-room/aulas-05/02-parametros.js
+++ class-room/aulas-05/02-parametros.js
@@ -15,3 +15,18 @@ function sum(a, b) {
 
 sum(12, 22)
 sum(11, 99)
+
+
+function joinText(text1, text2, text3) {
+    console.log(text1, text2, text3)
+}
+// Passando apenas um valor, os outros Parâmetros ficaram "undefined"
+joinText("erika")
+
+
+// Definindo um valor (argumento) padrão
+function joinTexts(text1, text2 = "", text3 = "") {
+    console.log(text1, text2, text3)
+}
+
+joinTexts("ZX")
