1e37e207a4054875b8a5ac4ef3dad43baa8253e9
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-05-15 22:59:35 -0300

    Create 02-parametros.js

---
+++ class-room/aulas-05/02-parametros.js
@@ -0,0 +1,17 @@
+// Parâmetros é a variável (escopo da função)
+function message(username) {
+    console.log("Olá", username)
+}
+
+// Argumento é o valor que passado para a função
+message("Rodrigo")
+message("Edson")
+
+
+
+function sum(a, b) {
+    console.log(a + b)
+}
+
+sum(12, 22)
+sum(11, 99)
