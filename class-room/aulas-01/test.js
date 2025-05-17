96747b6bc501327010b2a5eef47354dcc271c1f0
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-05-16 22:29:43 -0300

    teste-02

--- class-room/aulas-01/test.js
+++ class-room/aulas-01/test.js
@@ -1,33 +1,41 @@
 
-    let button = document.querySelector(".generalBtn")
-    let counterSpan = document.querySelector(".counter")
-    let totalSpan = document.querySelector(".total")
+let button = document.querySelector(".generalBtn")
+let counterSpan = document.querySelector(".counter")
+let totalSpan = document.querySelector(".total")
 
-    let count = 0
+let count = 0
 
-    button.addEventListener("click", () => {
-        button.classList.toggle("checked")
-        button.classList.toggle("default")
-    })
-    
-    button.addEventListener("click", (e) => {
+button.addEventListener("click", (e) => {
 
-        if (button.classList.contains("checked")) {
-            if (e.target.id === "img-02") {
+    if (button.classList.contains("checked")) {
+        if (e.target.id === "img-02") {
             count++
-            } else if (e.target.id === "img-01") {
-                if (count > 0) {
-                count--
-                }  
-            }
-            
-            counterSpan.textContent = count
-            totalSpan.textContent = count
-
-            if (count === 0) {
-                button.classList.remove("checked")
-                button.classList.add("default")
-            }
+        } else if (e.target.id === "img-01") {
+            if (count > 0) {
+            count--
+            }  
         }
-    })
+        
+        counterSpan.textContent = count
+        totalSpan.textContent = count
 
+        if (count === 0) {
+            button.classList.remove("checked")
+            button.classList.add("default")
+            counterSpan.textContent = ""
+        }
+
+    } else {
+        button.classList.add("checked")
+        button.classList.remove("default")
+        count = 1
+        counterSpan.textContent = count
+        totalSpan.textContent = count
+    }
+})
+
+
+// TESTE 02
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.querySelectorAll(".geralBtn")
+})
