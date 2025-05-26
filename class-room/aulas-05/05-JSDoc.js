5b95ab102f4f93e1ff79b41df49f8d8334bc2843
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-05-25 23:34:18 -0300

    Create 05-JSDoc.js

---
+++ class-room/aulas-05/05-JSDoc.js
@@ -0,0 +1,20 @@
+/* 
+O JSDoc é um padrão para incorporar documentação no código-fonte a partir desses comentários.
+*/
+
+/**
+ * Authenticates the user
+ * 
+ * @param {String} email user email.
+ * @param {String} password more than 6 characters.
+ * @returns {Number} user id.
+ */
+
+
+function signIn(email, password) {
+    // Fluxo de autenticação do usuário.
+
+    return 7
+}
+
+signIn("erika@email.com", "123456")
