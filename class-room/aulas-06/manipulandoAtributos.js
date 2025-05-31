7c9bda5187c20d4eb032532dbf375daed09cf219
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-05-30 22:53:25 -0300

    manipulando atributos - setAttribute

---
+++ class-room/aulas-06/manipulandoAtributos.js
@@ -0,0 +1,9 @@
+let input = document.querySelector("input")
+
+// Atualizar um atributo.
+//input.setAttribute("disabled", true)
+//input.setAttribute("type", "file")
+
+// Remover um atributo.
+//input.removeAttribute("id")
+
