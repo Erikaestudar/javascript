fb347fae9071c7ee857191e764d734f6d765bd43
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-09 17:48:18 -0300

    criando, acessando e atualizando um Objeto

--- class-room/aulas-08/objeto.js
+++ class-room/aulas-08/objeto.js
@@ -16,7 +16,7 @@ const user = {
     age: 18,
     // Estrutura de objeto aninhada
     name: {
-        first_name: "Erika",
+        first_name: "Edson",
         surname: "Sugawara"
     },
     address: {
@@ -26,8 +26,51 @@ const user = {
         postal_code: "19068-700"
     },
     message: () => {
-        console.log("hello! world")
+        console.log(`hello! ${user.name.first_name}`)
+    },
+    task: function () {
+        console.log(`Eu moro na ${this.address.street}, no estado de ${this.address.city} `)
     }
 }
 
-console.log(user)
+// Acessando Objetos.
+user.message()
+user.task()
+
+const userName = {
+    name: "Erika Miyuki",
+    email: "ka@email.com",
+    message: function () {
+        // Palavra reservada (this), o this não funciona com arrow function "() => {}". Deve-se usar o função anônima
+        console.log(`Olá ${this.name}, ${this.email}`)
+    },
+}
+
+// Acessando Objetos.
+userName.message()
+
+
+
+// ATUALIZANDO O OBJETO.
+const product = {
+    name: "Teclado",
+    quantity: 100,
+}
+
+
+
+
+// Atualiza o valor de uma propriedade.
+console.log(product.quantity) // Acessando a propriedade do objeto.
+product.quantity = 90
+console.log(product.quantity) 
+
+
+console.log(product.name)
+product.name = "Mouse"
+console.log(product.name)
+
+
+// Notação de colchetes.
+product["quantity"] = 50
+console.log(product)
