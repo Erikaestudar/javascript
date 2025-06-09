ca6b6efe5464aebe9101f1ff5de38dfa38b991fa
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-09 17:47:43 -0300

    criando repetições

--- class-room/aulas-01/test-02/script.js
+++ class-room/aulas-01/test-02/script.js
@@ -2,11 +2,13 @@ let oneBtn = document.querySelector("#firstBtn")
 let count = document.querySelector(".counter")
 let minus = document.querySelector("#img-01")
 let plus = document.querySelector("#img-02")
+let total = document.querySelector(".total")
 
 let counter = 1
 
 function updateCounter() {
     count.textContent = counter
+    total.textContent = counter
 }
 
 function plusMath() {
@@ -25,6 +27,10 @@ oneBtn.addEventListener("click", (event) => {
     event.preventDefault()
 
     oneBtn.classList.add("checked")
+
+    if (counter === 0) {
+        counter = 1 // Reinicia o contador quando ele estava zerado
+    }
     updateCounter()
 })
 
@@ -32,9 +38,12 @@ minus.addEventListener("click", (event) => {
     event.stopPropagation()
     if (counter >= 1) {
         minusMath()
-    } else {
+    } 
+    
+    if (counter === 0) {
         oneBtn.classList.remove("checked")
         count.textContent = ""
+        total.textContent = "0"
     }
 })
 
@@ -43,3 +52,33 @@ plus.addEventListener("click", (event) => {
     plusMath()
 })
 
+
+// CONFIGURAÇÃO DO FORM REPETIÇÕES E CRIAÇÃO DE ELEMENTOS
+
+let form = document.querySelector("form")
+let button = document.querySelector("#formBtn")
+
+let list = document.querySelector("ol.list")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    for (let i = 1; i <= 2; i++) {
+        let newItem = document.createElement("li")
+        newItem.classList.add("show-result")
+        newItem.textContent = `${i}`
+
+        let del = document.createElement("span")
+        del.classList.add("del")
+
+        newItem.appendChild(del)
+
+        list.appendChild(newItem)
+
+        del.addEventListener("click", (event) => {
+            event.stopPropagation()
+
+            list.removeChild(newItem)
+        })
+    }
+})
