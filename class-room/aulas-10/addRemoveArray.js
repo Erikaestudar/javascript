261e795d397a448f20a13d47b46e3db4d5731471
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-11 18:55:59 -0300

    push e pop, unshift e shift

---
+++ class-room/aulas-10/addRemoveArray.js
@@ -0,0 +1,21 @@
+let users = []
+
+// Adiciona um item ao final do Array.
+users.push("Erika")
+users.push("Edson")
+users.push("Elena")
+
+
+// Adiciona um item no início do Array.
+users.unshift("Ana")
+console.log(users)
+
+
+// Remove o item do início do Array.
+users.shift()
+console.log(users)
+
+
+// Remove do final do Array.
+users.pop()
+console.log(users)
