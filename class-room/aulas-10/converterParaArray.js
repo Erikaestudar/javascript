f9d20a1b421a474ce5852d21cf11ddee37a992dc
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-11 18:42:12 -0300

    convertendo String em Array

---
+++ class-room/aulas-10/converterParaArray.js
@@ -0,0 +1,7 @@
+let fullName = "Rodrigo Gonçalves Santana"
+
+// Cria um Array com os nomes separando pelo espaço.
+console.log(fullName.split(" "))
+
+// Cria um Array com as letras.
+console.log(Array.from(fullName))
