fc2ff4458ae8d93a3eed8b05f1f0e8ca1096522a
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-11 18:15:58 -0300

    array

---
+++ class-room/aulas-10/array.js
@@ -0,0 +1,11 @@
+// Criando Array com construtor.
+// [] - Array
+// {} - Object
+const newArray = new Array()
+console.log(newArray.length)
+
+
+// Cria a Array com 10 posições vazias.
+const available = new Array(10)
+console.log(available)
+console.log(available.length)
