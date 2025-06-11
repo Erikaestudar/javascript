eb50466802619cc939a95a3137fac9f81f051bb4
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-11 17:23:56 -0300

    lista de tarefas simples

--- class-room/aulas-01/test-03/script.js
+++ class-room/aulas-01/test-03/script.js
@@ -2,10 +2,7 @@ let form = document.querySelector("form")
 let newTask = document.querySelector("input#task")
 newTask.focus()
 
-let listWrapper = document.querySelector(".list-wrapper")
-
 let list = document.querySelector("ul.list")
-
 
 function capitalize(title) {
     return title.charAt(0).toUpperCase()+title.slice(1).toLowerCase() 
@@ -15,26 +12,10 @@ function capitalize(title) {
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
-
+   
     if (newTask.value.trim()) {
-        let capTask = capitalize(newTask.value)
-
-        
-        // Elementos do título.
-        let titleContainer = document.createElement("div")
-        titleContainer.classList.add("show-tasks")
-
-        let titleTask = document.createElement("h1")
-        titleTask.classList.add("title-task")
-        
-        titleTask.textContent =` ${capTask}`
-
-        let plusTask = document.createElement("span")
-        plusTask.classList.add("plus-task")       
-        
 
         // Criando os elementos da lista <li>.
-        
         let newItem = document.createElement("li")
         newItem.classList.add("show-tasks")
 
@@ -43,6 +24,7 @@ form.addEventListener("submit", (event) => {
 
         let taskName = document.createElement("p")
         taskName.classList.add("task-name")
+        taskName.textContent = capitalize(newTask.value)
 
         let editTask = document.createElement("span")
         editTask.classList.add("edit-task")
@@ -59,18 +41,6 @@ form.addEventListener("submit", (event) => {
         list.appendChild(newItem)
         list.classList.add("show-tasks")
 
-        for (let i = 1; i <= 5; i++) {
-            
-        }
-
-        titleContainer.appendChild(titleTask)
-        titleContainer.appendChild(plusTask)
-        titleContainer.appendChild(list)
-
-        listWrapper.appendChild(titleContainer)
-        listWrapper.classList.add("show.tasks")
-
-        
         del.addEventListener("click", (event) => {
             event.stopPropagation()
             list.removeChild(newItem)
@@ -85,13 +55,8 @@ form.addEventListener("submit", (event) => {
 
         */
         newTask.value = ""
-
     } else {
         alert(`[ERRO] Por favor, escreva uma nova tarefa!`)
     }  
+    
 })
-
-
-
-
-
