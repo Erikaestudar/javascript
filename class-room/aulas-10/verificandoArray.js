867a54d9b4254454b94b36a66f0fed7ec39d485a
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-11 19:38:30 -0300

    método includes

---
+++ class-room/aulas-10/verificandoArray.js
@@ -0,0 +1,10 @@
+let fruits = [
+    "Apple",
+    "Orange",
+    "Banana",
+]
+
+// Verifica se um item existe no Array.
+console.log(fruits.includes("Apple"))
+
+console.log(fruits.includes("Strawberry"))
