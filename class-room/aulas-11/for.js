bfcacf9fd050b50c644dd43fe880c165b7fe1f8b
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-15 19:16:49 -0300

    loop for

---
+++ class-room/aulas-11/for.js
@@ -0,0 +1,11 @@
+/* 
+    FOR repete até a codição especificada seja FALSA.
+
+    - variável de controle
+    - condição
+    - incremento ou decremento da variável de controle
+*/
+
+for (step = 0; step < 10; step++) {
+    console.log(step)
+}
