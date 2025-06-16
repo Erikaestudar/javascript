a935aeb94614309a87e52a54266d24781fd3e363
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-16 15:56:14 -0300

    loop for

--- class-room/aulas-11/for.js
+++ class-room/aulas-11/for.js
@@ -1,11 +1,21 @@
 /* 
     FOR repete até a codição especificada seja FALSA.
 
-    - variável de controle
-    - condição
-    - incremento ou decremento da variável de controle
+    - variável de controle;
+    - condição;
+    - incremento ou decremento da variável de controle;
 */
 
+
+/*
 for (step = 0; step < 10; step++) {
     console.log(step)
+}
+*/
+
+
+// Exemplo de tabuada.
+let number = 7
+for (i = 0; i <= 10; i++) {
+    console.log(`${number} x ${i} =  ${number * i}`)
 }
