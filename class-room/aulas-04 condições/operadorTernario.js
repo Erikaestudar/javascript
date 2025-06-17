61f6ce6b13ad1976d99fa5a97b2379dda4416004
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-17 20:43:18 -0300

    operador ternário

---
+++ class-room/aulas-04 condições/operadorTernario.js
@@ -0,0 +1,10 @@
+// Operador condicional ternário
+
+let age = 18 
+console.log(
+    age >= 18 // Condição
+    ? "Você pode dirigir." // Se verdadeiro
+    : "Você não pode dirigir." // Se falso
+)
+
+console.log(age < 18 ? "Você não vota." : "Voto obrigatório.")
