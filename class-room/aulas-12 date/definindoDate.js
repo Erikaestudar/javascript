5e729b18a0fd05992b49ae075f5c7681adb2a1f5
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-18 20:00:38 -0300

    Obtendo e definindo data e hora

---
+++ class-room/aulas-12 date/definindoDate.js
@@ -0,0 +1,10 @@
+// Define com ano, mês (0-11), dia.
+console.log(new Date(2025, 6, 18)) 
+
+// Define ano, mês (0-11), dia e data e hora, minuto e segundo.
+console.log(new Date(2025, 5, 18, 19, 30, 0))
+
+// Definindo data e hora com String.
+console.log(new Date("2024-06-18T14:30:00"))
+
+console.log(new Date("July 3, 2025 9:20:00"))
