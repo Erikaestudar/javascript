542a18d0a1d8e8db332cf949795eaa06539bd836
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-18 18:11:25 -0300

    atualizando

--- class-room/aulas-04 condições/tryCatchFinally.js
+++ class-room/aulas-04 condições/tryCatchFinally.js
@@ -1,1 +1,30 @@
 // Tratamento de Exceções
+/*
+try {
+    // TENTA executar algo.
+    console.log(result)
+
+} catch (error) {
+    //CAPTURA o erro para tratar.
+    
+
+    console.log("Não foi possível executar seu pedido. Tente novamente mais tarde!")
+
+    //console.log(error)
+} finally {
+    // Independente do resultado o finally vai ser executado.
+    console.log("Fim")
+}
+*/
+
+let result = 0
+
+try {
+    if (result === 0){
+        throw new Error("O valor é igual a zero.")
+    }
+} catch (error) {
+    console.log(error)
+} finally {
+    console.log("Fim")
+}
