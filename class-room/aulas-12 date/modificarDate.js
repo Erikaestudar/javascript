0e00cd8d17e6eeeacda3b5925e06034ec9c9c2d1
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-18 21:27:42 -0300

    métodos para trabalhar com data e hora

---
+++ class-room/aulas-12 date/modificarDate.js
@@ -0,0 +1,21 @@
+let date = new Date("july 3, 2025 14:30:00")
+
+// Modificar o ano.
+date.setFullYear(2030)
+
+// Modificar o mês (começa com 0).
+date.setMonth(7)
+
+// Modificar o dia.
+date. setDate(26)
+
+// Modificar a hora.
+date.setHours(21)
+
+// Modificar o minuto.
+date.setMinutes(50)
+
+// Modificar o segundo.
+
+date.setSeconds(44)
+console.log(date)
