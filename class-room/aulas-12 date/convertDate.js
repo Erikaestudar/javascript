ae0d7a0c3fd8c2c27a023de1add1fcd2698584e5
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-06-18 21:44:36 -0300

    convertendo para String específica

---
+++ class-room/aulas-12 date/convertDate.js
@@ -0,0 +1,11 @@
+let date = new Date("2025-07-26T09:10:00")
+
+console.log(date.toString())
+
+// Existem conversões específicas para date e hora.
+
+// Retorna somente a data.
+console.log(date.toDateString())
+
+// Retorna somente a hora.
+console.log(date.toTimeString())
