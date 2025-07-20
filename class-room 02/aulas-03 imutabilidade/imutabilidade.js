23ceaff84886137a63797d02381c92ed30d9c9f7
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-07-20 11:49:36 -0300

    Imutabilidade

--- class-room 02/aulas-03 imutabilidade/imutabilidade.js
+++ class-room 02/aulas-03 imutabilidade/imutabilidade.js
@@ -3,12 +3,29 @@ const address1 = {
     number: 20,
 }
 
-// Nessa caso esta substituindo.
+// Isso não é uma cópia é uma referência. Nesse caso esta substituindo.
 //const address2 = address1
 
-// Criando uma nova cópia para mudar um dado.
+// Criando uma nova cópia (um novo objeto utilizando as propriedades e valores de address1) para mudar um dado (Opção 1). 
+
+/*
 const address2 = {...address1}
 address2.number = 30
+*/
 
-console.log(address1)
-console.log(address2)
+// Opção 2
+const address2 = {...address1, number: 30}
+console.log(address1, address2)
+
+
+const list1 = ["Apple", "Banana"]
+
+// Exemplo de referência de Array 
+//const list2 = list1
+//list2.push("Watermelon")
+
+
+const list2 = [...list1, "Watermelon"]
+//list2.push("Watermelon")
+
+console.log(list1, list2)
